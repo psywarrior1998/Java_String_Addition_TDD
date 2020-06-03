09afee7cecc48095e088745da930e40f9d4c4bcf
Sanyam Sharma <sanyamsharma252@gmail.com>
2020-06-03 17:17:46 +0530

    Test Case with User defined or Custom Delimiter

--- StringAdd.java
+++ StringAdd.java
@@ -6,45 +6,105 @@ class WithFunction{
 		String temp="0";
 		// variable to store the sum
 		int sum=0;
-		for(int i = 0; i < number.length(); i++) 
-		{ 
-			char ch = number.charAt(i); 
-			try{
-				// if current character is a digit			
-				if (Character.isDigit(ch)){
-					//the number is added to the number in temp 
-					// so that multiple digit numbers aren't missed.
-					temp += ch;
+		
+		if(number.charAt(0)== '/' && number.charAt(1)== '/'){
+			String delim="";
+			int j=2;
+			while(number.charAt(j)!='\\' && number.charAt(j+1)!='n'){
+				delim+=number.charAt(j);
+				j+=1;
+			}
+			j+=2;
+			for(int i = j; i < number.length(); i++) 
+			{ 
+				char ch = number.charAt(i); 
+				try{
+					// if current character is a digit			
+					if (Character.isDigit(ch)){
+						//the number is added to the number in temp 
+						// so that multiple digit numbers aren't missed.
+						temp += ch;
+					}
+					// if current character is starting letter of deliminator
+					else if (ch==delim.charAt(0) && Character.isDigit(number.charAt(i+(delim.length()))))
+					{
+						int cter = 1;
+						for(int k=1;k<delim.length(); k++)
+						{
+							if(number.charAt(i+k)==delim.charAt(k)){
+								cter += 1;
+							}
+							else{
+								break;
+							}
+						}
+						if(cter == delim.length()){
+							// increment sum by number found earlier
+							sum += Integer.parseInt(temp);
+							// reset the temp variable for the next integer
+							temp = "0";
+							i+=2;
+						}
+						else{
+							//if the charecter is something other than the deliminator
+							sum=-1;
+							temp="0";
+							break;
+						}
+					}	
+					else{
+						//if the charecter is something other than the deliminator
+						sum=-1;
+						temp="0";
+						break;
+					}
 				}
-				// if current character is a comma 
-				else if (ch==',')
-				{ 
-					// increment sum by number found earlier (if any)
-					sum += Integer.parseInt(temp);
-					
-					// reset the temp variable for the next integer
-					temp = "0";
-				}
-				// if the charecter is '\' then check if it is succeded by n 
-				// and then a number then add it to the sum
-				else if (ch=='\\' && number.charAt(i+1)=='n' && Character.isDigit(number.charAt(i+2))){
-					i+=1;
-					sum += Integer.parseInt(temp);
-					// reset the temp variable for the next integer
-					temp = "0";
-				}
-				else{
-					//if the charecter is something other than number or comma
-					sum=-1;
-					temp="0";
-					break;
+				catch(StringIndexOutOfBoundsException e){
+					System.out.println("deliminators must be succeded by numbers");
 				}
 			}
-			catch(StringIndexOutOfBoundsException e){
-				System.out.println("deliminators must be succeded by numbers");
+			
+		}
+		else{
+			for(int i = 0; i < number.length(); i++) 
+			{ 
+				char ch = number.charAt(i); 
+				try{
+					// if current character is a digit			
+					if (Character.isDigit(ch)){
+						//the number is added to the number in temp 
+						// so that multiple digit numbers aren't missed.
+						temp += ch;
+					}
+					// if current character is a comma 
+					else if (ch==',' && Character.isDigit(number.charAt(i+1)))
+					{ 
+						// increment sum by number found earlier (if any)
+						sum += Integer.parseInt(temp);
+						
+						// reset the temp variable for the next integer
+						temp = "0";
+					}
+					// if the charecter is '\' then check if it is succeded by n 
+					// and then a number then add it to the sum
+					else if (ch=='\\' && number.charAt(i+1)=='n' && Character.isDigit(number.charAt(i+2))){
+						i+=1;
+						sum += Integer.parseInt(temp);
+						// reset the temp variable for the next integer
+						temp = "0";
+					}
+					else{
+						//if the charecter is something other than number or comma
+						sum=-1;
+						temp="0";
+						break;
+					}
+				}
+				catch(StringIndexOutOfBoundsException e){
+					System.out.println("deliminators must be succeded by numbers");
+				}
 			}
 		}
-		
 		sum += Integer.parseInt(temp);
 		//to add the last digit eg 1,2 then this will add 2 to the sum
 		return sum;
